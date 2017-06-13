c20827dccec0d5e5d353ba39a030c6167dbbc113
Davidwucd <r04943042@ntu.edu.tw>
2017-06-13 15:42:48 +0800

    Create imdb_lstm.py

---
+++ imdb_lstm.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import numpy as np
+import glob
+np.random.seed(42)  # for reproducibility
+
+from keras.preprocessing import sequence
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Embedding
+from keras.layers import LSTM, SimpleRNN, GRU
+from keras.datasets import imdb
+from keras.utils import plot_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+max_features = 20000
+maxlen = 80  # cut texts after this number of words (among top max_features most common words)
+batch_size = 32
+test_ratio = 0.6
+
+print('Loading data...')
+(X_train, y_train), (X_Test, y_Test) = imdb.load_data(num_words=max_features)
+
+X_test = X_Test[:int(test_ratio*len(X_Test))]
+X_validation = X_Test[int(test_ratio*len(X_Test)):]
+y_test = y_Test[:int(test_ratio*len(y_Test))]
+y_validation = y_Test[int(test_ratio*len(y_Test)):]
+
+print(len(X_train), 'train sequences')
+print(len(X_validation), 'validation sequences')
+print(len(X_test), 'test sequences')
+
+print('Pad sequences (samples x time)')
+X_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+X_validation = sequence.pad_sequences(X_validation, maxlen=maxlen)
+X_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+print('X_train shape:', X_train.shape)
+print('X_validation shape:', X_validation.shape)
+print('X_test shape:', X_test.shape)
+
+print('Build model...')
+model = Sequential()
+model.add(Embedding(max_features, 128, dropout=0.2))
+model.add(LSTM(128, dropout_W=0.2, dropout_U=0.2))  # try using a GRU instead, for fun
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+# try using different optimizers and different optimizer configs
+model.compile(loss='binary_crossentropy',
+              optimizer='Adam',
+              metrics=['accuracy'])
+print(model.summary())
+plot_model(model, to_file='model_lstm.png')
+
+model_fname = 'model_lstm.hdf5'
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=5, verbose=0, mode='auto')
+model_checkpoint = ModelCheckpoint(model_fname, monitor='val_loss', verbose=1, save_best_only=True, save_weights_only=True, mode='auto', period=1)
+callbacks=[early_stopping, model_checkpoint]
+
+print('Train...')
+model.fit(X_train, y_train, batch_size=batch_size, epochs=100,
+          validation_data=(X_validation, y_validation), callbacks=callbacks)
+score, acc = model.evaluate(X_test, y_test,
+                            batch_size=batch_size)
+print('Test score:', score)
+print('Test accuracy:', acc)
